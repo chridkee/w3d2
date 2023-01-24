3fb7ba60131fc47ba74ca68d06bc70c5abcd6739
“Lissa <lwarshaw674@gmail.com>
2023-01-24 12:15:55 -0500

    first update

---
+++ card.rb
@@ -0,0 +1,33 @@
+class Card 
+
+
+
+  
+def initialize(face_value,position)
+  @face_value
+  @position 
+
+end
+
+def get_infortmation
+  
+  if @position == true 
+    @face_value
+  end
+
+
+end
+
+
+def hide
+  
+end
+
+def reveal
+
+end
+
+
+
+
+end
