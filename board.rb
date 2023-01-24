9d3804722f4b5dce70015ad2573b2d9c7aa88b01
chridkee <christopher.keesor@gmil.com>
2023-01-24 14:00:22 -0500

    board update 1

--- board.rb
+++ board.rb
@@ -1,2 +1,24 @@
 # require_relative "card"
  
+class Board 
+
+    def initialize
+        @grid = Array.new(4) {Array.new(4, " ")}
+    end 
+
+    def populate
+        
+    end 
+
+    def grid
+      @grid
+    end
+end
+
+p Board.new.grid
+
+
+
+letters = ("a".."h").to_a
+
+p letters 
--- card.rb
+++ card.rb
@@ -1,33 +1,28 @@
 class Card 
 
-
-
   
-def initialize(face_value,position)
-  @face_value
-  @position 
-
+def initialize(letter,face)
+  @letter = letter 
+  @face = face
 end
 
-def get_infortmation
-  
-  if @position == true 
+def show
+  if @face == true 
     @face_value
   end
-
-
 end
 
 
 def hide
-  
-end
-
-def reveal
+  if @face == true
+#show hidden grid index? 
 
 end
 
 
 
 
+
 end
+
+"_"
